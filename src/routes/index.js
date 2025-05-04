b0d313f135005b756e30a7902bc3294d09614d43
Code_NooB <alexangel77719@gmail.com>
2025-05-04 22:20:50 +0300

    fix: rename proxyRouter to fundingRatesRouter for consistency

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const proxyRouter = require('./fundingRates');
+const fundingRatesRouter    = require('./fundingRates');
 const verifyToken = require('../middlewares/verifyToken');
 
 // Захищаємо всі /api/proxy/* запити
